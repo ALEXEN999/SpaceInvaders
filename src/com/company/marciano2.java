34c4856f7c4eaaa7398cafdb3a139cd6a7f58a64
sbravor64 <33894589+sbravor64@users.noreply.github.com>
2019-02-26 17:45:22 +0100

    Add files via upload
    marciano 2

---
+++ src/com/company/marciano2.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class marciano2 {
+    int x,y;
+    int w,h;
+
+    void pintar(){
+        Canvas.fillRect(x+w,y+h,w,h);
+        Canvas.fillRect(x+w*2,y+h,w,h);
+        Canvas.fillRect(x+w*3,y+h,w,h);
+        Canvas.fillRect(x+w*4,y+h,w,h);
+        Canvas.fillRect(x+w*5,y+h,w,h);
+        Canvas.fillRect(x+w,y+h*2,w,h);
+        Canvas.fillRect(x+w*3,y+h*2,w,h);
+        Canvas.fillRect(x+w*5,y+h*2,w,h);
+        Canvas.fillRect(x+w,y+h*3,w,h);
+        Canvas.fillRect(x+w*2,y+h*3,w,h);
+        Canvas.fillRect(x+w*3,y+h*3,w,h);
+        Canvas.fillRect(x+w*4,y+h*3,w,h);
+        Canvas.fillRect(x+w*5,y+h*3,w,h);
+        Canvas.fillRect(x+w*3,y+h*4,w,h);
+        Canvas.fillRect(x+w,y+h*5,w,h);
+        Canvas.fillRect(x+w*2,y+h*5,w,h);
+        Canvas.fillRect(x+w*3,y+h*5,w,h);
+        Canvas.fillRect(x+w*4,y+h*5,w,h);
+        Canvas.fillRect(x+w*5,y+h*5,w,h);
+
+    }
+}
