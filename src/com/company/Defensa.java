ca380a6c42f1608b2393ab8048e99a97076ff3c7
ALEXEN999 <alexpf49@gmail.com>
2019-02-26 18:50:06 +0100

    juntado

--- src/com/company/Defensa.java
+++ src/com/company/Defensa.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Defensa {
+    // Campos  == variables
+    int x, y;
+    int w, h;
+
+    //Metodos
+    void pintar(){
+
+        Canvas.fillRect(x,y,w, h);
+        Canvas.fillRect(x,y+h,w,h);
+        Canvas.fillRect(x+w,y,w, h);
+        Canvas.fillRect(x+w*2,y,w, h);
+        Canvas.fillRect(x+w*2,y+h,w, h);
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -40,5 +40,95 @@ public class Main {
         live3.h = 15;
 
         live3.pintar();
+
+        Defensa barrera1 = new Defensa();
+
+        barrera1.x = 150;
+        barrera1.y = 450;
+        barrera1.w = 25;
+        barrera1.h = 25;
+
+        barrera1.pintar();
+
+        Defensa barrera2 = new Defensa();
+
+        barrera2.x = 250;
+        barrera2.y = 450;
+        barrera2.w = 25;
+        barrera2.h = 25;
+
+        barrera2.pintar();
+
+        Defensa barrera3 = new Defensa();
+
+        barrera3.x = 350;
+        barrera3.y = 450;
+        barrera3.w = 25;
+        barrera3.h = 25;
+
+        barrera3.pintar();
+
+        Defensa barrera4 = new Defensa();
+
+        barrera4.x = 450;
+        barrera4.y = 450;
+        barrera4.w = 25;
+        barrera4.h = 25;
+
+        barrera4.pintar();
+
+        Defensa barrera5 = new Defensa();
+
+        barrera5.x = 550;
+        barrera5.y = 450;
+        barrera5.w = 25;
+        barrera5.h = 25;
+
+        barrera5.pintar();
+
+        marciano2 marci1 = new marciano2();
+
+        marci1.x = 150;
+        marci1.y = 150;
+        marci1.w = 10;
+        marci1.h = 10;
+
+        marci1.pintar();
+
+        marciano2 marci2 = new marciano2();
+
+        marci2.x = 250;
+        marci2.y = 150;
+        marci2.w = 10;
+        marci2.h = 10;
+
+        marci2.pintar();
+
+        marciano2 marci3 = new marciano2();
+
+        marci3.x = 350;
+        marci3.y = 150;
+        marci3.w = 10;
+        marci3.h = 10;
+
+        marci3.pintar();
+
+        marciano2 marci4 = new marciano2();
+
+        marci4.x = 450;
+        marci4.y = 150;
+        marci4.w = 10;
+        marci4.h = 10;
+
+        marci4.pintar();
+
+        marciano2 marci5 = new marciano2();
+
+        marci5.x = 550;
+        marci5.y = 150;
+        marci5.w = 10;
+        marci5.h = 10;
+
+        marci5.pintar();
     }
 }
